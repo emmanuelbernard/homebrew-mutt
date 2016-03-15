2e777db21729add16d4afa19a5f05d6ced86c882
Emmanuel Bernard <emmanuel@hibernate.org>
2016-03-15 15:18:14 +0100

    Update mutt formula to 1.5.24 and include sidebar, trash and indexcolor patches
    Start from mutt 1.5.23 formula and update to 1.5.24 from known sources
    like OpenBSD
    Add trash and sidebar patches from OpenBSD sources
    Add indexcolor patch from x-way's work (from whom I forked)

--- Formula/mutt.rb
+++ Formula/mutt.rb
@@ -10,48 +10,60 @@
 
 class Mutt < Formula
   desc "Mongrel of mail user agents (part elm, pine, mush, mh, etc.)"
-  homepage "http://www.mutt.org/"
-  url "https://bitbucket.org/mutt/mutt/downloads/mutt-1.5.24.tar.gz"
-  mirror "ftp://ftp.mutt.org/pub/mutt/mutt-1.5.24.tar.gz"
+  homepage 'http://www.mutt.org/'
+  url 'https://mirrors.kernel.org/debian/pool/main/m/mutt/mutt_1.5.24.orig.tar.gz'
+  mirror 'https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mutt/mutt_1.5.24.orig.tar.gz'
   sha256 "a292ca765ed7b19db4ac495938a3ef808a16193b7d623d65562bb8feb2b42200"
 
-  head do
-    url "http://dev.mutt.org/hg/mutt#default", :using => :hg
+  bottle do
+    revision 3
+    sha256 "7dcb549c1a9efc7f4c1a0602709478cae6f071215a762c77b4d9fba1ab561287" => :yosemite
+    sha256 "4b9b8c3a3b069d6103f650f1136161d41de1bc6014a682b6c15fd8d562cc773b" => :mavericks
+    sha256 "79b84411d1f0916e7df520b9fbac9f9d4479a4631f5da6b616f363fd6f68d0c2" => :mountain_lion
+  end
 
-    resource "html" do
-      url "http://dev.mutt.org/doc/manual.html", :using => :nounzip
+  head do
+    url 'http://dev.mutt.org/hg/mutt#default', :using => :hg
+
+    resource 'html' do
+      url 'http://dev.mutt.org/doc/manual.html', :using => :nounzip
     end
   end
 
-  unless Tab.for_name("signing-party").with? "rename-pgpring"
-    conflicts_with "signing-party",
-      :because => "mutt installs a private copy of pgpring"
+  unless Tab.for_name('signing-party').with? 'rename-pgpring'
+    conflicts_with 'signing-party',
+      :because => 'mutt installs a private copy of pgpring'
   end
 
-  conflicts_with "tin",
-    :because => "both install mmdf.5 and mbox.5 man pages"
+  conflicts_with 'tin',
+    :because => 'both install mmdf.5 and mbox.5 man pages'
 
   option "with-debug", "Build with debug option enabled"
   option "with-trash-patch", "Apply trash folder patch"
+  option "with-sidebar-patch", "Apply sidebar patch"
   option "with-indexcolor-patch", "Apply indexcolor patch"
   option "with-s-lang", "Build against slang instead of ncurses"
   option "with-ignore-thread-patch", "Apply ignore-thread patch"
+  option "with-pgp-verbose-mime-patch", "Apply PGP verbose mime patch"
   option "with-confirm-attachment-patch", "Apply confirm attachment patch"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
-  depends_on "openssl"
-  depends_on "tokyo-cabinet"
-  depends_on "s-lang" => :optional
-  depends_on "gpgme" => :optional
+  depends_on 'openssl'
+  depends_on 'tokyo-cabinet'
+  depends_on 's-lang' => :optional
+  depends_on 'gpgme' => :optional
 
-  if build.with? "trash-patch"
-    patch do
-      url "https://blog.x-way.org/stuff/mutt-1.5.24-trash_folder.diff"
-      sha256 "985d7f6ae17e15e525b19e348b3a43e78177cea3b775434abcbe7d220bebe934"
-    end
-  end
+  patch do
+    url "http://ftp.fr.openbsd.org/pub/OpenBSD/distfiles/mutt/trashfolder-1.5.24.diff.gz"
+    sha256 "395ffaa6517306e5f5e7327a1aacf3a5cadb34c5b6483170e3f314c95fdb5cb7"
+  end if build.with? "trash-patch"
+
+  patch do
+    url "http://ftp.fr.openbsd.org/pub/OpenBSD/distfiles/mutt/sidebar-1.5.24.diff.gz"
+    sha256 "75f208279ad2a6fe7066dee97ef6fa56a9f0e663c2d94bcb069cda91bd7a4ccf"
+  end if build.with? "sidebar-patch"
 
   if build.with? "indexcolor-patch"
     patch do
@@ -62,25 +74,27 @@ class Mutt < Formula
 
   # original source for this went missing, patch sourced from Arch at
   # https://aur.archlinux.org/packages/mutt-ignore-thread/
-  if build.with? "ignore-thread-patch"
-    patch do
-      url "https://gist.githubusercontent.com/mistydemeo/5522742/raw/1439cc157ab673dc8061784829eea267cd736624/ignore-thread-1.5.21.patch"
-      sha256 "7290e2a5ac12cbf89d615efa38c1ada3b454cb642ecaf520c26e47e7a1c926be"
-    end
-  end
+  patch do
+    url "https://gist.githubusercontent.com/mistydemeo/5522742/raw/1439cc157ab673dc8061784829eea267cd736624/ignore-thread-1.5.21.patch"
+    sha1 "dbcf5de46a559bca425028a18da0a63d34f722d3"
+  end if build.with? "ignore-thread-patch"
 
-  if build.with? "confirm-attachment-patch"
-    patch do
-      url "https://gist.githubusercontent.com/tlvince/5741641/raw/c926ca307dc97727c2bd88a84dcb0d7ac3bb4bf5/mutt-attach.patch"
-      sha256 "da2c9e54a5426019b84837faef18cc51e174108f07dc7ec15968ca732880cb14"
-    end
-  end
+  patch do
+    url "http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.2+deb7u1/features-old/patch-1.5.4.vk.pgp_verbose_mime"
+    sha1 "a436f967aa46663cfc9b8933a6499ca165ec0a21"
+  end if build.with? "pgp-verbose-mime-patch"
+
+  patch do
+    url "https://gist.githubusercontent.com/tlvince/5741641/raw/c926ca307dc97727c2bd88a84dcb0d7ac3bb4bf5/mutt-attach.patch"
+    sha1 "94da52d50508d8951aa78ca4b073023414866be1"
+  end if build.with? "confirm-attachment-patch"
+
 
   def install
     args = ["--disable-dependency-tracking",
             "--disable-warnings",
             "--prefix=#{prefix}",
-            "--with-ssl=#{Formula["openssl"].opt_prefix}",
+            "--with-ssl=#{Formula['openssl'].opt_prefix}",
             "--with-sasl",
             "--with-gss",
             "--enable-imap",
@@ -88,18 +102,19 @@ class Mutt < Formula
             "--enable-pop",
             "--enable-hcache",
             "--with-tokyocabinet",
-            # This is just a trick to keep 'make install' from trying
-            # to chgrp the mutt_dotlock file (which we can't do if
-            # we're running as an unprivileged user)
+            # This is just a trick to keep 'make install' from trying to chgrp
+            # the mutt_dotlock file (which we can't do if we're running as an
+            # unpriviledged user)
             "--with-homespool=.mbox"]
-    args << "--with-slang" if build.with? "s-lang"
-    args << "--enable-gpgme" if build.with? "gpgme"
+    args << "--with-slang" if build.with? 's-lang'
+    args << "--enable-gpgme" if build.with? 'gpgme'
 
-    if build.with? "debug"
+    if build.with? 'debug'
       args << "--enable-debug"
     else
       args << "--disable-debug"
     end
+
 
     system "./prepare", *args
     system "make"
--- README.md
+++ README.md
@@ -1,19 +1,29 @@
 # homebrew-mutt
 
 This tap contains the mutt 1.5.24 formula with updated versions of patches no longer included in the official homebrew repository:
+
 - trash_folder patch
+- sidebar patch
 - indexcolor patch
+
+Tested with the following options:
+
+```
+brew install [.../ see below /...] --with-sidebar-patch --with-trash-patch --with-gpgme --with-s-lang
+```
 
 ## How do I install this formula?
 
-Just `brew tap x-way/mutt` and then `brew install x-way/mutt/mutt`.
+Just `brew tap emmanuelbernard/mutt` and then `brew install emmanuelbernard/mutt/mutt`.
 
 You can also install via URL:
+
 ```
-brew install https://raw.githubusercontent.com/x-way/homebrew-mutt/master/Formula/mutt.rb
+brew install https://raw.githubusercontent.com/emmanuelbernard/homebrew-mutt/master/Formula/mutt.rb
 ```
 
 ## Troubleshooting
+
 First, please run `brew update` and `brew doctor`.
 
 Second, read the [Troubleshooting Checklist](https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Troubleshooting.md#troubleshooting).
@@ -25,4 +35,5 @@ Second, read the [Troubleshooting Checklist](https://github.com/Homebrew/homebre
 `brew help`, `man brew` or check [the documentation](https://github.com/Homebrew/homebrew/tree/master/share/doc/homebrew#readme).
 
 ## License
-Code is under the [BSD 2 Clause (NetBSD) license](https://github.com/x-way/homebrew-mutt/tree/master/LICENSE.txt).
+
+Code is under the [BSD 2 Clause (NetBSD) license](https://github.com/emmanuelbernard/homebrew-mutt/tree/master/LICENSE.txt).
